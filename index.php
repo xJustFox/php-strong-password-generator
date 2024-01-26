0ebbdd3f761fe9ffc4a416f2c14c5e0330d5bc28
xJustFox <dodydaversa@gmail.com>
2024-01-26 14:01:48 +0100

    [Upgrade] Added forms and 'if' control in php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    if (isset($_GET['psw_leng']) && $_GET['psw_leng'] != '' ) {
+        function pswGen() {
+            
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="text-end m-4 ">
+        <form action="./index.php" method="get">
+            <input type="text" name="psw_leng" id="pswLeng" placeholder="Lunghezza Password">
+            <button type="submit">Genera</button>
+        </form>
+    </div>
+</body>
+</html>
